2d7b69b65c1d90bffbc927bc6d7f2f10c25635ae
mahisha <mahisha@mahishas-MacBook-Pro.local>
2025-12-08 23:00:43 -0500

    infra eks updates

--- eks/terraform/providers.tf
+++ eks/terraform/providers.tf
@@ -4,66 +4,16 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      # You can tighten this if you want, but this lets Terraform pick a compatible 6.x
-      # version that satisfies module constraints.
+      # Let Terraform pick a compatible 6.x version for the modules
       # version = ">= 6.23.0, < 7.0.0"
-    }
-
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.26"
-    }
-
-    helm = {
-      source  = "hashicorp/helm"
-      version = "~> 2.13"
     }
   }
 }
 
-########################################
-# AWS provider
-########################################
 provider "aws" {
   region = var.region
 }
 
+# These are used by IAM policy for ALB controller
 data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
-
-########################################
-# EKS cluster data for K8s/Helm providers
-# (uses outputs from module "eks")
-########################################
-
-# Name of the cluster comes from the EKS module
-# (module "eks" must be defined in main.tf)
-data "aws_eks_cluster" "this" {
-  name = module.eks.cluster_name
-}
-
-data "aws_eks_cluster_auth" "this" {
-  name = module.eks.cluster_name
-}
-
-########################################
-# Kubernetes provider (talks to EKS)
-########################################
-
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.this.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.this.token
-}
-
-########################################
-# Helm provider shares the same auth
-########################################
-
-provider "helm" {
-  kubernetes {
-    host                   = data.aws_eks_cluster.this.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.this.token
-  }
-}
